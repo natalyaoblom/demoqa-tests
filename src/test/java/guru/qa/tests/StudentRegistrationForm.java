7173c00a797248c6f9cf4e0c08c2560f30e5c3f4
Natalya <natalyaoblom@mail.ru>
2021-12-29 21:06:57 +0300

    delete //

--- src/test/java/guru/qa/tests/StudentRegistrationForm.java
+++ src/test/java/guru/qa/tests/StudentRegistrationForm.java
@@ -28,36 +28,19 @@ public class StudentRegistrationForm {
         $("#userEmail").setValue("email@email.com");
 
         $("#genterWrapper").$(byText("Male")).click();
-//        $("#gender-radio-1").parent().click(); // doubleClick();
-//        $("label[for=gender-radio-1]").click(); //без label тоже можно и [for='...']
-//       $(byText("Other")).click(); //BAD PRACTICE
 
         $("#userNumber").setValue("9009999099");
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1995");
-//        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-//        $$(".react-datepicker__day--030") // $ -  SelenideElement. $$ - ElementsCollection;
-//                .filter(not(cssClass(".react-datepicker__day--outside-month)")))
-//                .first() // or .get(0)
-//                .click();
-//        $("[aria-label=\"Choose Sunday, April 30th, 1995\"]"); //квадратные скобки пара ключ:параметр
-//        $("[aria-label='Choose Sunday, April 30th, 1995'");
-//        $("[aria-label=Choose Sunday, April 30th, 1995");// NOT WORKING
-        $("[aria-label$='April 30th, 1995']").click(); //заканчивается с '...'
-//        $x("//*contains(@aria-label, \"April 30th, 1995\"]").click();
 
-//<div class="react-datepicker__day      react-datepicker__day--030 react-datepicker__day--outside-month"  aria-label="Choose Thursday, March 30th, 1995" >30</div>
-//<div class="react-datepicker__day      react-datepicker__day--030 react-datepicker__day--weekend"        aria-label="Choose Sunday, April 30th, 1995" >30</div>
+        $("[aria-label$='April 30th, 1995']").click();
 
         $("#subjectsInput").setValue("English").pressEnter();
 
         $("#hobbiesWrapper").$(byText("Reading")).click();
 
-//        $("#uploadPicture").uploadFile(new File("src/test/resources/images/1.png"));
-//        File one = new File("src/test/resources/images/1.png");
-//        $("#uploadPicture").uploadFile(one);
         $("#uploadPicture").uploadFromClasspath("images/1.png");
 
         $("#currentAddress").setValue("Some address");
